a8f4326c016cf3d162642387467c94b24ab1dad1
kowalkowskim <kowalkowskim@student.mini.pw.edu.pl>
2017-05-05 21:27:37 +0200

    Add Reflection.cs :)

---
+++ robot/Reflection.cs
@@ -0,0 +1,38 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace robot
+{
+    class Reflection : AnimatedObject
+    {
+        private Robot robot;
+        private Mesh rectangle;
+
+        public Reflection(Robot robot, Mesh rectangle)
+        {
+            this.robot = robot;
+            this.rectangle = rectangle;
+        }
+
+        public override void DoAnimation(float deltaTime)
+        {
+            for (int i = 0; i < meshes.Count(); i++)
+            {
+                meshes[i].ModelMatrix = robot.meshes[i].ModelMatrix *
+                    rectangle.InvertedModelMatrix *
+                    Matrix4.CreateScale(new Vector3(1, 1, -1)) *
+                    rectangle.ModelMatrix;
+            }
+        }
+
+        public override Mesh[] ProvideObjectMeshes()
+        {
+            MeshLoader ml = new MeshLoader();
+            return ml.GetRobotMesh();
+        }
+    }
+}
